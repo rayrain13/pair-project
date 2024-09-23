df7ea88a9a9d10a412f6dab20307b3f3b443c65a
rayrain13 <dirtin3@outlook.com>
2024-09-23 18:29:27 +0800

    feat: 表达式类Expression

--- src/main/java/ArithmeticProblemGenerator.java
+++ src/main/java/ArithmeticProblemGenerator.java
@@ -1,5 +1,17 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Random;
+
 public class ArithmeticProblemGenerator {
-    class Fraction {
+    private static final Random random = new Random();
+    private static final int MAX_OPERATORS = 3;
+
+
+
+    static class Fraction {
         int numerator;
         int denominator;
 
@@ -60,4 +72,70 @@ public class ArithmeticProblemGenerator {
             return this.numerator * other.denominator < other.numerator * this.denominator;
         }
     }
+
+    static class Expression {
+        Fraction value;
+        String repr;
+
+        public Expression(Fraction value, String repr) {
+            this.value = value;
+            this.repr = repr;
+        }
+    }
+
+    private static Fraction generateNumber(int maxValue) {
+        if (random.nextBoolean()) {
+            return new Fraction(random.nextInt(maxValue - 1) + 1, 1);
+        } else {
+            int numerator = random.nextInt(maxValue - 1) + 1;
+            int denominator = random.nextInt(maxValue - numerator) + numerator + 1;
+            return new Fraction(numerator, denominator);
+        }
+    }
+
+    private static Expression generateExpression(int maxValue, int remainingOperators) {
+        if (remainingOperators == 0 || random.nextDouble() < 0.3) {
+            Fraction num = generateNumber(maxValue);
+            return new Expression(num, num.toString());
+        }
+
+        Expression left = generateExpression(maxValue, remainingOperators - 1);
+        Expression right = generateExpression(maxValue, remainingOperators - 1);
+        char op = "+-×÷".charAt(random.nextInt(4));
+
+        Fraction result;
+        String expr;
+
+        switch (op) {
+            case '+':
+                result = left.value.add(right.value);
+                expr = String.format("(%s + %s)", left.repr, right.repr);
+                break;
+            case '-':
+                if (left.value.isLessThan(right.value)) {
+                    Expression temp = left;
+                    left = right;
+                    right = temp;
+                }
+                result = left.value.subtract(right.value);
+                expr = String.format("(%s - %s)", left.repr, right.repr);
+                break;
+            case '×':
+                result = left.value.multiply(right.value);
+                expr = String.format("(%s × %s)", left.repr, right.repr);
+                break;
+            case '÷':
+                if (right.value.numerator == 0 || !left.value.divide(right.value).isLessThan(new Fraction(1, 1))) {
+                    return generateExpression(maxValue, remainingOperators);
+                }
+                result = left.value.divide(right.value);
+                expr = String.format("(%s ÷ %s)", left.repr, right.repr);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + op);
+        }
+
+        return new Expression(result, expr);
+    }
+
 }
